910bb877d236a24ddc367a1cf77f6f203e7bb429
BRAVO68WEB <jbandyopadhayaya@gmail.com>
2020-09-12 09:57:18 +0530

    Fixes

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
+// var inquirer = require('inquirer');
 console.log("Hello from DF Community API");
+//var inquirer = require('inquirer');
+//inquirer
+//  .prompt([
+ //   {
+   // type: 'input',
+ //   name: 'welcome',
+   // message: "Do you want to Login or Register?",
+ //   }
+ // ])
+  //.then(answers => {
+    
+  //})
+ // .catch(error => {
+ //  if(error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+  //  } else {
+      // Something else when wrong
+  //  }
+ // });
